0619773f53d72f495a6f394a121b7d2ea7fec3f3
Gabor Reszler <hello@gaborreszler.dev>
2024-01-10 14:02:58 +0100

    modify method used in service seeder in order to persist fake-generated models

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -13,6 +13,6 @@ class ServiceSeeder extends Seeder
      */
     public function run(): void
     {
-        Service::factory()->count(50)->make();
+        Service::factory()->count(50)->create();
     }
 }
