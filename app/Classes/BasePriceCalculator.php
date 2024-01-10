99713d0edde7f4a765959d38e255bb6c43c8b505
Gabor Reszler <hello@gaborreszler.dev>
2024-01-10 14:29:20 +0100

    add property to base price calculator class

--- app/Classes/BasePriceCalculator.php
+++ app/Classes/BasePriceCalculator.php
@@ -2,9 +2,12 @@
 
 namespace App\Classes;
 
+use App\Models\Service;
 use Illuminate\Support\Carbon;
 
 abstract class BasePriceCalculator
 {
+    protected Service $service;
+
     public function calculate(Carbon $startDate, Carbon $endDate, int $serviceId) {}
 }
